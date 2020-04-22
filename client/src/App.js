4501558d319f536593d2b7dfbf3945bb2323268a
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2020-04-22 02:21:11 -0500

    adding footer to the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import Navbar from "./component/Navbar/index"
+import Navbar from "./component/Navbar/index";
+import Footer from "./component/Footer/index"
 
 
 function App() {
   return (
+    <React.Component>
+      <Navbar/>
+      <Footer/>
+    </React.Component>
 
-    <Navbar/>
+
+
 
   );
 }
---
+++ client/src/component/Footer/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faFilePdf, faEnvelope, faPhone  } from '@fortawesome/free-solid-svg-icons'
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+        <div className="row">
+            <div className="col-1-of-3">
+                <div className="footer__navigation">
+                    <ul className="footer__list">
+                        <li className="footer__item"> <a href="https://github.com/elhamposhtiban" className="footer__link"><FontAwesomeIcon icon={faGithub} size="3x" className="media-link"/></a> </li>
+                        <li className="footer__item"> <a href="https://www.linkedin.com/in/elham-poshtiban-2a40a4149/" className="footer__link"><FontAwesomeIcon icon={faLinkedin} size="3x" className="media-link"/></a> </li>
+                        <li className="footer__item"> <a href="../assets/images/ElhamPoshtiban Tech-Resume.pdf" className="footer__link"><FontAwesomeIcon icon={faFilePdf} size="3x" className="media-link"/></a> </li>
+                        <li className="footer__item"> <a href="mailto:elham.poshtiban@gmail.com" className="footer__link"><FontAwesomeIcon icon={faEnvelope} size="3x" className="media-link"/></a> </li>
+                        <li className="footer__item"> <a href="tel:2814259682" className="footer__link"><FontAwesomeIcon icon={faPhone} size="3x" className="media-link"/></a> </li>
+                    </ul> 
+                </div>
+            </div>
+            <div className="col-2-of-3">
+                <p className="footer__copyright">
+                     copyright&copy;by Elham Poshtiban.
+                     <br/>
+                      you are 100% allowed to use this webpage for both personal and commercial use but NOT to claim it as your own design.A credit to author, Elham poshtiban, is of course highly appreciated!
+                </p>
+            </div>
+        </div>
+    </footer>
+    )
+}
+
+export default Footer;
