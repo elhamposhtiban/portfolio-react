8b3f5ec8621b4d6e0316882084ed97f6e80ea2d3
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2020-04-22 02:40:36 -0500

    adding react dom to the package.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./component/Navbar/index";
 import Footer from "./component/Footer/index"
 
 
 function App() {
   return (
-    <React.Component>
-      <Navbar/>
-      <Footer/>
-    </React.Component>
+
+      <Router>
+    
+      <Navbar />
+
+        {/* <Route exact path="/" component={About} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/discover" component={Discover} />
+        <Route exact path="/search" component={Search} /> */}
+
+      <Footer />
+   
+      </Router>
 
 
 
