152db0fa6b20b496179e5b435024c505ab95f1d5
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2020-05-02 23:22:47 -0500

    add tag link to navbar

--- client/src/component/Navbar/index.js
+++ client/src/component/Navbar/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 const Navbar = () => {
@@ -13,9 +14,15 @@ const Navbar = () => {
         <div className="navigation__background"></div>
         <nav className="navigation__nav">
             <ul className="navigation__list">
-                <li className="navigation__item"><a href="/bio" className="navigation__link">home</a></li>
-                <li className="navigation__item"><a href="/contact" className="navigation__link">contact</a></li>
-                <li className="navigation__item"><a href="/portfolio" className="navigation__link">portfolio</a></li>
+                <li className="navigation__item">
+                <Link to="/bio" className="navigation__link">Home</Link>
+                </li>
+                <li className="navigation__item">
+                <Link to="/contact" className="navigation__link">contact</Link>
+                </li>
+                <li className="navigation__item">
+                <Link to="/portfolio" className="navigation__link">portfolio</Link>
+                </li>
             </ul>
         </nav>
     </div>
